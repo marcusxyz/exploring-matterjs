e02ba020555eb635fa59a52a35e6a678973b2efd
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2021-12-05 15:12:51 +0100

    added basic shortcuts, height and width of canvas to js

---
+++ shapes.js
@@ -0,0 +1,19 @@
+// Two things matter needs
+
+//*******************************
+// 01 An engine
+
+//*******************************
+// 02 A renderer
+
+// alias is a shortcut for making the code cleaner
+// const Engine = Matter.Engine;
+// const Render = Matter.Render;
+const { Engine, Render, Bodies, World } = Matter;
+
+// Where is matter being deployed
+const sectionTag = document.querySelector("section.shapes");
+
+// Canvas height and width
+const w = window.innerWidth;
+const h = window.innerHeight;
