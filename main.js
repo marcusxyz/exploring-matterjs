b5a486e7ad5de3bf0369498b8e0b40e0a63f02a0
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2021-12-05 10:45:10 +0100

    setup matterjs

---
+++ main.js
@@ -0,0 +1,26 @@
+
+// Shortcuts
+
+const timescale = () => {
+  const Engine = Matter.Engine,
+  const Render = Matter.Render,
+  const Runner = Matter.Runner,
+  const Body = Matter.Body,
+  const Events = Matter.Events,
+  const Compsite = Matter.Composite,
+  const Composites = Matter.Composites,
+  const Common = Matter.Common,
+  MouseConstraint = Matter.MouseConstraint,
+  Mouse = Matter.Mouse,
+  Bodies = Matter.Bodies;
+
+  // create engine
+  const engine = Engine.create();
+  const world = engine.world;
+
+  // create render
+  const render = Render.create({
+
+  })
+
+}
