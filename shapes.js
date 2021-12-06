82aecb96b055a0e185acaaac9f224a7d267443d9
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2021-12-06 13:45:13 +0100

    added different types of shapes

--- shapes.js
+++ shapes.js
@@ -9,7 +9,17 @@
 // alias is a shortcut for making the code cleaner
 // const Engine = Matter.Engine;
 // const Render = Matter.Render;
-const { Engine, Render, Bodies, World } = Matter;
+const {
+  Engine,
+  Render,
+  Bodies,
+  World,
+  MouseConstraint,
+  Composite,
+  Composites,
+  Common,
+  Runner,
+} = Matter;
 
 // Where is matter being deployed
 const sectionTag = document.querySelector("section.shapes");
@@ -30,18 +40,94 @@ const renderer = Render.create({
   },
 });
 
-// Create shapes
+//Create shapes
+
 const createShape = (x, y) => {
-  return Bodies.circle(x, y, 20 + 1 * Math.random());
+  const sides = Math.round(Common.random(1, 8));
+
+  // round the edges of some bodies
+  const chamfer = null;
+  if (sides > 2 && Common.random() > 0.7) {
+    chamfer = {
+      radius: 10,
+    };
+  }
+
+  switch (Math.round(Common.random(0, 1))) {
+    case 0:
+      if (Common.random() < 0.8) {
+        return Bodies.rectangle(
+          x,
+          y,
+          Common.random(25, 50),
+          Common.random(25, 50),
+          { chamfer: chamfer }
+        );
+      } else {
+        return Bodies.rectangle(
+          x,
+          y,
+          Common.random(80, 120),
+          Common.random(25, 30),
+          { chamfer: chamfer }
+        );
+      }
+    case 1:
+      return Bodies.polygon(x, y, sides, Common.random(25, 50), {
+        chamfer: chamfer,
+      });
+  }
 };
 
-// When we click the page add a new shape
+// Big ball in middle
+
+const bigBall = Bodies.circle(w / 2, h / 2, h / 5, {
+  // shape options
+  isStatic: true,
+  render: {
+    fillStyle: "#fff",
+  },
+});
+
+const wallOptions = {
+  isStatic: true,
+  render: {
+    visible: false,
+  },
+};
+
+const ground = Bodies.rectangle(w / 2, h + 50, w + 100, 100, wallOptions);
+const ceiling = Bodies.rectangle(w / 2, -50, w + 100, 100, wallOptions);
+const leftWall = Bodies.rectangle(-50, h / 2, 100, h + 100, wallOptions);
+const rightWall = Bodies.rectangle(w + 50, h / 2, 100, h + 100, wallOptions);
+
+const mouseControl = MouseConstraint.create(engine, {
+  element: sectionTag,
+  constraint: {
+    render: {
+      visible: false,
+    },
+  },
+});
+
+// Add bodies to world to see results
+World.add(engine.world, [
+  bigBall,
+  ground,
+  ceiling,
+  leftWall,
+  rightWall,
+  mouseControl,
+]);
+
+// Shapes spawn based on mousemove or click
 document.addEventListener("mousemove", function (event) {
   const shape = createShape(event.pageX, event.pageY);
   World.add(engine.world, shape);
 });
 
 // run both the engine and the renderer
+engine.gravity.y = 0.05;
 
 Engine.run(engine);
 Render.run(renderer);
